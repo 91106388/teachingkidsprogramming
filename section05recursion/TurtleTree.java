57ebf8f95dfa70fde5f9ef9d7b6a7b327d839c20
91106388 <91106388@g.mvusd.net>
2016-11-16 08:24:30 -0800

    TurtleTree

--- section05recursion/TurtleTree.java
+++ section05recursion/TurtleTree.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.util.HashMap;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class TurtleTree
 {
@@ -11,8 +12,9 @@ public class TurtleTree
   public static void main(String[] args)
   {
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#10
+    Tortoise.setSpeed(10);
     //    Turn the background black  --#21
+    Tortoise.getBackgroundWindow().setBackground(black)
     int branchLength = 60;
     //
     drawBranch(branchLength);
@@ -30,28 +32,38 @@ public class TurtleTree
       //            A 30 pixel long branch is dark green --#18
       //            A 40 pixel long branch is olive --#17
       //            A 50 pixel long branch is sienna --#14
+      colors.put(50, PenColors.Browns.Sienna);
       //            A 60 pixel long branch is saddle brown (TIP: Put the values into the 'colors' HashMap)--#13 
+      colors.put(60, PenColors.Browns.SaddleBrown);
       //            Get the value of the branch length from the 'colors' HashMap and use that to set the pen color --#21
       //        ------------- End of adjustColor --#15.3
       //
       Tortoise.move(branchLength);
       //        drawLowerBranches (recipe below) --#6.1
       //
-      //        ------------- Recipe for drawLowerBranches --#6.2
-      Tortoise.turn(30);
-      //            drawShorterBranch (recipe below) --#8.1
-      //
-      //            ------------- Recipe for drawShorterBranch --#8.2
-      drawBranch(branchLength - 10);
-      //            ------------- End of drawShorterBranch recipe --#8.3
-      //
-      //            Turn the Tortoise 60 degrees to the left --#7
-      //            drawShorterBranch --#9
-      //            Turn the Tortoise 30 degrees to the right --#12
-      //            adjustColor --#16
-      //            Move the tortoise backward the length of the current branch --#11
-      //        ------------- End of drawLowerBranches recipe --#6.3
+      drawLowerBranches(branchLength);
       //
     }
+  }
+  private static void drawLowerBranches(int branchLength)
+  {
+    //        ------------- Recipe for drawLowerBranches --#6.2
+    Tortoise.turn(30);
+    //            drawShorterBranch (recipe below) --#8.1
+    drawShorterBranch(branchLength);
+    //
+    Tortoise.turn(-60);
+    drawShorterBranch(branchLength);
+    Tortoise.turn(30);
+    //            adjustColor --#16
+    Tortoise.move(-60);
+    //        ------------- End of drawLowerBranches recipe --#6.3
+  }
+  private static void drawShorterBranch(int branchLength)
+  {
+    //
+    //            ------------- Recipe for drawShorterBranch --#8.2
+    drawBranch(branchLength - 10);
+    //            ------------- End of drawShorterBranch recipe --#8.3
   }
 }
